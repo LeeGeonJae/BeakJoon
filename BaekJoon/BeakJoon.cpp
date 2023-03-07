aff05ba01a31e36d9ca0feed1b99ec6ae119130a
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-07 21:34:40 +0900

    백준 11021번 문제 : A+B - 7

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,8 +1,13 @@
+//문제
+//두 정수 A와 B를 입력받은 다음, A + B를 출력하는 프로그램을 작성하시오.
+//
 //입력
-//첫 줄에 테스트케이스의 개수 T가 주어진다.T는 최대 1, 000, 000이다.다음 T줄에는 각각 두 정수 A와 B가 주어진다.A와 B는 1 이상, 1, 000 이하이다.
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 //
 //출력
-//각 테스트케이스마다 A + B를 한 줄에 하나씩 순서대로 출력한다.
+//각 테스트 케이스마다 "Case #x: "를 출력한 다음, A + B를 출력한다.테스트 케이스 번호는 1부터 시작한다.
 
 #include <iostream>
 
@@ -10,19 +15,16 @@ using namespace std;
 
 int main()
 {
-	cin.tie(NULL);
-	std::ios_base::sync_with_stdio(false);
-
 	int Count;
 	int a;
 	int b;
 
 	cin >> Count;
 
-	for (int i = 0; i < Count; i++)
+	for (int i = 1; i <= Count; i++)
 	{
 		cin >> a >> b;
-		cout << a + b << '\n';
+		cout << "Case #" << i << ": " << a + b << '\n';
 	}
 
 	return 0;
