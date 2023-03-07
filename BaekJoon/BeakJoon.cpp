56ef64ae8c34d1ca9f3fdd268f60cc6670d1d54d
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-07 22:41:50 +0900

    백준 문제 10807번 : 개수 세기

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,26 +1,42 @@
 //문제
-//두 정수 A와 B를 입력받은 다음, A + B를 출력하는 프로그램을 작성하시오.
+//총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
 //
 //입력
-//입력은 여러 개의 테스트 케이스로 이루어져 있다.
-//
-//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.둘째 줄에는 정수가 공백으로 구분되어져있다셋째 줄에는 찾으려고 하는 정수 v가 주어진다.입력으로 주어지는 정수와 v는 - 100보다 크거나 같으며, 100보다 작거나 같다.
 //
 //출력
-//각 테스트 케이스마다 A + B를 출력한다.
+//첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int a, b;
+    int Count;
+    int Find;
+    int Sum = 0;
 
-    while (cin >> a >> b)
+    cin >> Count;
+    vector<int> a;
+
+    for (int i = 0; i < Count; i++)
     {
-        cout << a + b << endl;
+        int b;
+        cin >> b;
+        a.push_back(b);
     }
+
+    cin >> Find;
+
+    for (int i = 0; i < Count; i++)
+    {
+        if (a[i] == Find)
+            Sum++;
+    }
+
+    cout << Sum << endl;
 
     return 0;
 }
