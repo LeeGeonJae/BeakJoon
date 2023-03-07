8a7bb6d3485a505f0d13a5cdfa8a145ea12c97f6
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-07 22:01:48 +0900

    백준 문제 10952번 : A+B - 5

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,13 +1,15 @@
 //문제
-//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-//
-//하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+//두 정수 A와 B를 입력받은 다음, A + B를 출력하는 프로그램을 작성하시오.
 //
 //입력
-//첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+//입력은 여러 개의 테스트 케이스로 이루어져 있다.
+//
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//입력의 마지막에는 0 두 개가 들어온다.
 //
 //출력
-//첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+//각 테스트 케이스마다 A + B를 출력한다.
 
 #include <iostream>
 
@@ -15,21 +17,19 @@ using namespace std;
 
 int main()
 {
-	int Count;
+	int a;
+	int b;
+	bool Zero = false;
 
-	cin >> Count;
-
-	for (int i = 1; i <= Count; i++)
+	while (Zero == false)
 	{
-		for (int q = 0; q < Count - i; q++)
+		cin >> a >> b;
+		if (a == 0 && b == 0)
 		{
-			cout << " ";
+			Zero = true;
+			break;
 		}
-		for (int j = Count - i; j < Count; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
+		cout << a + b << endl;
 	}
 
 	return 0;
