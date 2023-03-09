dd1756807a6b4656580dace079eed6007580c6dc
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-09 22:47:08 +0900

    백준 문제 11718번 : 그대로 출력하기

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,48 +1,19 @@
-//상근이의 동생 상수는 수학을 정말 못한다.상수는 숫자를 읽는데 문제가 있다.이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다.상근이는 세 자리 수 두 개를 칠판에 써주었다.그 다음에 크기가 큰 수를 말해보라고 했다.
+//문제
+//입력 받은 대로 출력하는 프로그램을 작성하시오.
 //
-//상수는 수를 다른 사람과 다르게 거꾸로 읽는다.예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다.따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
-//
-//두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+//입력
+//입력이 주어진다.입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다.또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-int main()
-{
-    string First;
-    string Second;
-    string High;
-
-    char temp;
-
-    cin >> First >> Second;
-
-    temp = First.front();
-    First[0] = First.back();
-    First[2] = temp;
-
-    temp = Second.front();
-    Second[0] = Second.back();
-    Second[2] = temp;
-
-    for (int i = 0; i < 3; i++)
+int main() {
+    string line;
+    while (getline(cin, line)) 
     {
-        if ((int)First[i] < (int)Second[i])
-        {
-            High = Second;
-            break;
-        }
-        else if ((int)First[i] > (int)Second[i])
-        {
-            High = First;
-            break;
-        }
+        cout << line << endl;
     }
-
-    cout << High << endl;
-
     return 0;
 }
