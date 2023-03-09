263d4bf2ae0a99c0f1de2ba0735af3804c065b4a
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-09 22:25:29 +0900

    백준 문제 2908번 : 상수

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,42 +1,48 @@
-//문제
-//총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+//상근이의 동생 상수는 수학을 정말 못한다.상수는 숫자를 읽는데 문제가 있다.이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다.상근이는 세 자리 수 두 개를 칠판에 써주었다.그 다음에 크기가 큰 수를 말해보라고 했다.
 //
-//입력
-//첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.둘째 줄에는 정수가 공백으로 구분되어져있다셋째 줄에는 찾으려고 하는 정수 v가 주어진다.입력으로 주어지는 정수와 v는 - 100보다 크거나 같으며, 100보다 작거나 같다.
+//상수는 수를 다른 사람과 다르게 거꾸로 읽는다.예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다.따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
 //
-//출력
-//첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+//두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main()
 {
-    int Count;
-    int Find;
-    int Sum = 0;
+    string First;
+    string Second;
+    string High;
 
-    cin >> Count;
-    vector<int> a;
+    char temp;
 
-    for (int i = 0; i < Count; i++)
+    cin >> First >> Second;
+
+    temp = First.front();
+    First[0] = First.back();
+    First[2] = temp;
+
+    temp = Second.front();
+    Second[0] = Second.back();
+    Second[2] = temp;
+
+    for (int i = 0; i < 3; i++)
     {
-        int b;
-        cin >> b;
-        a.push_back(b);
+        if ((int)First[i] < (int)Second[i])
+        {
+            High = Second;
+            break;
+        }
+        else if ((int)First[i] > (int)Second[i])
+        {
+            High = First;
+            break;
+        }
     }
 
-    cin >> Find;
-
-    for (int i = 0; i < Count; i++)
-    {
-        if (a[i] == Find)
-            Sum++;
-    }
-
-    cout << Sum << endl;
+    cout << High << endl;
 
     return 0;
 }
