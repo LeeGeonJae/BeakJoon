4259b80e8373f28009beebcb66430c79dc5437b8
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-10 23:59:39 +0900

    백준 문제 풀이 : 알파벳 많이 쓰인 문자 출력. 가장 많이 쓰인 알파벳이 여러개이면 ? 출력

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,19 +1,44 @@
-//문제
-//입력 받은 대로 출력하는 프로그램을 작성하시오.
-//
-//입력
-//입력이 주어진다.입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다.또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main() {
-    string line;
-    while (getline(cin, line)) 
+int main()
+{
+    int Alphabet[26] = {};
+    int Most = 0;
+    int Count = 0;
+    bool MostDouble;
+    string Word;
+
+    cin >> Word;
+
+    for (int i = 0; i < Word.length(); i++)
     {
-        cout << line << endl;
+        if (Word[i] >= 'a')
+            Alphabet[Word[i] - 'a']++;
+        else
+            Alphabet[Word[i] - 'A']++;
     }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (Count < Alphabet[i])
+        {
+            Count = Alphabet[i];
+            Most = i;
+            MostDouble = false;
+        }
+        else if (Count == Alphabet[i])
+            MostDouble = true;
+    }
+
+    if (MostDouble)
+        cout << "?" << endl;
+    else if (Most >= 26)
+        cout << (char)(Most - 26 + 'A') << endl;
+    else
+        cout << (char)(Most + 'A') << endl;
+
     return 0;
 }
