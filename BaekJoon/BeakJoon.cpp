3bd22fe74751e29c41bc41806dbf573220cd1c37
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-19 19:05:48 +0900

    백준 문제 2738번 : 행렬 덧셈

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -6,49 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>> A;
-    vector<vector<int>> B;
-    vector<int> C;
-    vector<vector<int>> Sum;
-    int Number;
-    int Size;
-    int Size2;
 
-    cin >> Size >> Size2;
-
-    for (int i = 0; i < Size; i++)
-    {
-        A.push_back(C);
-        for (int j = 0; j < Size2; j++)
-        {
-            cin >> Number;
-            A[i].push_back(Number);
-        }
-    }
-
-    for (int i = 0; i < Size; i++)
-    {
-        B.push_back(C);
-        for (int j = 0; j < Size2; j++)
-        {
-            cin >> Number;
-            B[i].push_back(Number);
-
-        }
-    }
-
-    for (int i = 0; i < Size; i++)
-    {
-        Sum.push_back(C);
-
-        for (int j = 0; j < Size2; j++)
-        {
-            Sum[i].push_back(B[i][j] + A[i][j]);
-
-            cout << Sum[i][j] << " ";
-        }
-        cout << endl;
-    }
 
     return 0;
 }
