f35da517f1e3e770eb5b81653b4fe51ee69fc970
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-19 19:33:19 +0900

    백준 문제 2566번 : 최댓값

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -6,7 +6,30 @@ using namespace std;
 
 int main()
 {
+    int Number[9][9] = {};
+    int NumberInput;
+    int BigNumber = 0;
+    int Width;
+    int High;
 
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cin >> NumberInput;
+            Number[i][j] = NumberInput;
+
+            if (BigNumber < Number[i][j])
+            {
+                BigNumber = Number[i][j];
+                Width = i + 1;
+                High = j + 1;
+            }
+        }
+    }
+
+    cout << BigNumber << endl;
+    cout << Width << " " << High << endl;
 
     return 0;
 }
