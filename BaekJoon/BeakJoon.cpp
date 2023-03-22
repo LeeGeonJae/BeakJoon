2e2bd5908725287a17b9097b7bb9ffcd98d61747
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-03-22 23:20:23 +0900

    백준 2581번 문제 : 소수 문제 풀이

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int main() 
-{
-    int Divisor;
-    int Number;
-    int Count = 1;
+int main() {
+    int MinNumber;
+    int MaxNumber;
+    int arr[10000] = {};
+    int Sum = 0;
+    vector<int> PrimeNumber;
 
-    cin >> Number >> Divisor;
+    cin >> MinNumber >> MaxNumber;
 
-    if (Number % Divisor == 0)
-    {
-        for (int i = 1; i < Divisor; i++)
-        {
-            if (Number % i == 0)
-                Count++;
-        }
+    if (MinNumber < 2) MinNumber = 2;
 
-        cout << Count << endl;
+    for (int i = 2; i <= MaxNumber; i++)
+        arr[i] = i;
+
+    for (int i = 2; i <= sqrt(MaxNumber); i++) {
+        if (arr[i] == 0) continue;
+        for (int j = 2 * i; j <= MaxNumber; j += i)
+            arr[j] = 0;
+    }
+
+    for (int i = MinNumber; i <= MaxNumber; i++) {
+        if (arr[i] != 0)
+            PrimeNumber.push_back(arr[i]);
+    }
+
+    for (int i = 0; i < PrimeNumber.size(); i++)
+        Sum += PrimeNumber[i];
+
+    if (PrimeNumber.size() > 0) {
+        cout << Sum << endl;
+        cout << PrimeNumber[0];
     }
     else
-        cout << "0" << endl;
+        cout << -1;
 
     return 0;
 }
