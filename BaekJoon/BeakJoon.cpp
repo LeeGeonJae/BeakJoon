683b5758037ce838c98b1df98a2d95d8a7b4e294
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-04-12 22:31:18 +0900

    프로그래머스 : 두 정수 사이의 합

--- BaekJoon/BeakJoon.cpp
+++ BaekJoon/BeakJoon.cpp
@@ -4,40 +4,23 @@
 
 using namespace std;
 
-int main() {
-    int MinNumber;
-    int MaxNumber;
-    int arr[10000] = {};
-    int Sum = 0;
-    vector<int> PrimeNumber;
+long long solution(int a, int b) 
+{
+    long long answer = 0;
 
-    cin >> MinNumber >> MaxNumber;
-
-    if (MinNumber < 2) MinNumber = 2;
-
-    for (int i = 2; i <= MaxNumber; i++)
-        arr[i] = i;
-
-    for (int i = 2; i <= sqrt(MaxNumber); i++) {
-        if (arr[i] == 0) continue;
-        for (int j = 2 * i; j <= MaxNumber; j += i)
-            arr[j] = 0;
-    }
-
-    for (int i = MinNumber; i <= MaxNumber; i++) {
-        if (arr[i] != 0)
-            PrimeNumber.push_back(arr[i]);
-    }
-
-    for (int i = 0; i < PrimeNumber.size(); i++)
-        Sum += PrimeNumber[i];
-
-    if (PrimeNumber.size() > 0) {
-        cout << Sum << endl;
-        cout << PrimeNumber[0];
-    }
+    if (a < b)
+        for (int i = a; i <= b; i++)
+            answer += i;
     else
-        cout << -1;
+        for (int i = b; i <= a; i++)
+            answer += i;
+
+    return answer;
+}
+
+int main() 
+{
+    cout << solution(3, 5);
 
     return 0;
 }
